0b19d4cf4da89cd264246b1e13a58a3d58df05d3
dereksmiley <32503817+dereksmiley@users.noreply.github.com>
2019-03-24 14:34:43 -0400

    Create variance.py

---
+++ variance.py
@@ -0,0 +1,44 @@
+
+data = [
+{"first" : "125", "last":"168"},
+{"first" : "142", "last":"120"},
+{"first" : "109", "last":"108"},
+{"first" : "97", "last":"89"},
+{"first" : "91", "last":"20"},
+{"first" : "40", "last":"45"},
+{"first" : "245", "last":"249"},
+{"first" : "387", "last":"386"},
+{"first" : "123", "last":"247"},
+{"first" : "12", "last":"6"},
+{"first" : "4", "last":"17"},
+]
+
+
+print("Frist".ljust(9, " ") +  "Last".ljust(9, " ") + "Varaince".ljust(10, " ") +  "Percent In Change" + '\n')
+firstTotal = 0
+secondTotal = 0
+varainceTotal = 0
+changePercentTotal = 0
+
+for obj in data:
+  firstNum =  int(obj['first'])
+  lastNum = int(obj['last'])
+  firstTotal += firstNum
+  secondTotal += lastNum
+  variance = firstNum - lastNum
+  changePercent = variance / firstNum  * 100
+
+
+  if variance < 0:
+    varianceString = "(" + str(abs(variance)) + ")"
+  else:
+    varianceString = str(variance)      
+
+  print(str(firstNum).ljust(9, " ") +  str(lastNum).ljust(9, " ") + varianceString.ljust(10, " ") +  str(round(changePercent, 2)) + '\n')
+
+varainceTotal = firstTotal - secondTotal
+changePercentTotal = varainceTotal / firstTotal * 100
+print("Totals")
+print("Frist".ljust(9, " ") +  "Last".ljust(9, " ") + "Varaince".ljust(10, " ") +  "Percent In Change" )
+print(str(firstTotal).ljust(9, " ") +  str(secondTotal).ljust(9, " ") + str(varainceTotal).ljust(10, " ") +  str(round(changePercentTotal, 2)) + '\n')
+
